61d28fbbb28c8f3908593e15fe8bb4b6932d275d
yaoba2000 <woyaoyan89@163.com>
2018-02-27 20:44:07 +0800

    测试

--- src/main/java/yao/algorithm/DoubleToIntFunctionTest.java
+++ src/main/java/yao/algorithm/DoubleToIntFunctionTest.java
@@ -11,9 +11,7 @@ import java.util.function.DoubleToIntFunction;
 public class DoubleToIntFunctionTest {
 
     public static void main(String[] args) {
-        DoubleToIntFunction df = (x) -> {
-            return (int) (x * 10) + 2;
-        };
+        DoubleToIntFunction df = (x) -> (int) (x * 10) + 2;
         System.out.println(df.applyAsInt(3.14));
         BinaryOperator<Integer> bi = BinaryOperator.minBy(Comparator.reverseOrder());//选出两个数中最大的
         System.out.println(bi.apply(2, 3));
